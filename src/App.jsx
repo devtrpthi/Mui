e67e9db2d96bdfeffc10efe84bb1cda7d83667bb
devtrpthi <deveshkrishnatripathi@gmail.com>
2022-06-19 17:27:45 +0530

    grid added in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,15 @@
 import React from "react";
 import {Typography,AppBar, Card, CardActions, CardContent, CssBaseline,Grid,Toolbar,Container} from '@mui/material';
 import { PhotoCamera } from "@mui/icons-material";
+import { Button } from '@mui/material';
 
-import Button from '@mui/material/Button';
-import ButtonGroup from '@mui/material/ButtonGroup';
-import Box from '@mui/material/Box';
 
 
 function App() {
     return(
         <>
             <CssBaseline />
-            <AppBar positon="relative">
+            <AppBar positon="sticky">
                 <Toolbar>
                     <PhotoCamera/>
                     <Typography variant="h6">
@@ -21,23 +19,30 @@ function App() {
             </AppBar>    
             <main>
                 <div>
-                    <Container maxwidth="sm">
-                        <Typography variant="h2" align="center" color="textPrimary" gutterBottom>
-                            Photo Album 
-                        </Typography>
-                        <Typography variant="h2" align="center" color="textPrimary" gutterBottom>
+                    <Container maxwidth="sm" style={{marginTop: '200px'}}>
+                        <Typography variant="h2" align="center" gutterBottom>
                             Photo Album 
                         </Typography>
                         <Typography variant="h5" align="center" color="textSecondary" paragraph>
                             Hello everyone This is a phto album and I'm trying to make this sentence as long as possible so we can see how does it look like on the screen.
                         </Typography>
+                        <div>
+                            <Grid container spacing={2} jsutify="cneter">
+                                <Grid item>
+                                        <Button variant="contained" color="primary">
+                                            See my photos
+                                        </Button>
+                                </Grid>
+                                <Grid item>
+                                        <Button variant="outlined" color="primary">
+                                            Secondary action
+                                        </Button>
+                                </Grid>
+                            </Grid>
+                        </div>
                     </Container>
                 </div>
-                <ButtonGroup variant="outlined" aria-label="outlined button group">
-        <Button>One</Button>
-        <Button>Two</Button>
-        <Button>Three</Button>
-      </ButtonGroup>
+                
             </main>
         </>
     );
