6dd5f63cc6c5dc1aee772bd0bd9dcf576869df25
devtrpthi <deveshkrishnatripathi@gmail.com>
2022-06-19 11:37:50 +0530

    added typography

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import {Typography} from '@mui/material';
 
 function App() {
     return(
         <div>
-        <h1>Hello World!</h1>
+        <Typography variant="h1">Hello World!</Typography>
         </div>
     );
 }
